484ba8a1da3084ea5c00094d425be1849646fbba
Scott <scrosehart@gmail.com>
2018-05-03 15:53:16 -0700

    continually block movement

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ if (window.innerWidth > 1000) {
   canvas.width = 500;
 }
 
-let block1 = {
-  x: 100,
-  y: 100,
-  color: '#3fa'
+class Block {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
 };
+
+let block1 = new Block(100, 100, '#3fa');
+let block2 = new Block(-50, 100, '#3fa');
 
 var moveBlock = e => {
   switch (e.keyCode) {
@@ -43,10 +48,29 @@ let animationLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = block1.color;
-  ctx.fillRect(block1.x, block1.y, 30, 30);
+  ctx.fillRect(block1.x, block1.y, 40, 30);
+  ctx.fillStyle = block2.color;
+  ctx.fillRect(block2.x, block2.y, 40, 30);
 
-  // if (block1.x <= )
-  // block1.x +=3;
+  if (block1.x <= 850 && block2.x < 0) {
+    console.log('one');
+    block1.x +=3 ;
+  } else if (block1.x > 850 && block2.x < 0) {
+    console.log('two');
+    block1.x += 3;
+    block2.x += 3;
+  } else if (block2.x <= 850) {
+    console.log('three');
+    block1.x = -50;
+    block2.x += 3;
+  } else if (block2.x > 850 && block1.x < 0) {
+    console.log('four');
+    block1.x += 3;
+    block2.x += 3;
+  } else if (block1.x <= 850) {
+    block1.x += 3;
+    block2.x = -50;
+  }
 
   animateGame = requestAnimationFrame(animationLoop);
 };
