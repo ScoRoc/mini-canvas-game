cb558791f7664abf7d2f639dc2ebbbb4a8baebb2
Scott <scrosehart@gmail.com>
2018-05-07 13:28:52 -0700

    block1 color change on y move

--- index.js
+++ index.js
@@ -79,56 +79,21 @@ let changeBlock = (e, block) => {
   }
 };
 
-// var moveBlock = e => {
-//   switch (e.keyCode) {
-//     case 38:  // Up
-//     e.preventDefault();
-//       block1.y -=3;
-//       break;
-//     case 40:  // Down
-//     e.preventDefault();
-//       block1.y +=3;
-//       break;
-//     case 37:  // Left
-//     e.preventDefault();
-//       block1.x -=3;
-//       break;
-//     case 39:  // right
-//     e.preventDefault();
-//       block1.x +=3;
-//       break;
-//   }
-// };
-
 let animateGame = null;
 
 let animationLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = block1.color;
+  block1.y > 200 ? ctx.fillStyle = block1.color : ctx.fillStyle = 'purple';
   ctx.fillRect(block1.x, block1.y, block1.width, block1.height);
   ctx.fillStyle = block2.color;
   ctx.fillRect(block2.x, block2.y, block2.width, block2.height);
 
-  // if (block1.x <= 850 && block2.x < 0) {
-  //   console.log('one');
-  //   block1.x +=3 ;
-  // } else if (block1.x > 850 && block2.x < 0) {
-  //   console.log('two');
-  //   block1.x += 3;
-  //   block2.x += 3;
-  // } else if (block2.x <= 850) {
-  //   console.log('three');
-  //   block1.x = -50;
-  //   block2.x += 3;
-  // } else if (block2.x > 850 && block1.x < 0) {
-  //   console.log('four');
-  //   block1.x += 3;
-  //   block2.x += 3;
-  // } else if (block1.x <= 850) {
-  //   block1.x += 3;
-  //   block2.x = -50;
-  // }
+  if (currentBlock === block1) {
+    canvas.style.backgroundColor = '#fafae0';
+  } else if (currentBlock === block2) {
+    canvas.style.backgroundColor = '#fef3c0';
+  }
 
   if (currentBlock.moving) {
     switch (currentBlock.moveDir) {
